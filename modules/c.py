346cb10c02f30b99c91a0a95f9150dfcbbeb12f6
kmackay <ken@s010600233295427c.vc.shawcable.net>
2013-02-06 23:20:48 -0800

    Added documentation to the c module.

--- modules/c.py
+++ modules/c.py
@@ -7,11 +7,38 @@ log = logging.getLogger("emk.c")
 utils = emk.module("utils")
 
 class _GccCompiler(object):
+    """
+    Compiler class for using gcc/g++ to compile C/C++ respectively.
+    
+    In order for the EMK c module to use a compiler instance, the compiler must define the following methods:
+      load_extra_dependencies
+      compile_c
+      compile_cxx
+    See the documentation for those functions in this class for more details.
+    """
     def __init__(self, path_prefix=""):
+        """
+        Create a new GccCompiler instance.
+        
+        Arguments:
+          path_prefix -- The prefix to use for the gcc/g++ executables. For example, if you had a 32-bit Linux cross compiler
+                         installed into /cross/linux, you might use 'c.compiler = c.GccCompiler("/cross/linux/bin/i686-pc-linux-gnu-")'
+                         to configure the c module to use the cross compiler. The default value is "" (ie, use the system gcc/g++).
+        """
         self.c_path = path_prefix + "gcc"
         self.cxx_path = path_prefix + "g++"
     
     def load_extra_dependencies(self, path):
+        """
+        Load extra dependencies from a dependency file that was written by this compiler instance's
+        compile_c() or compile_cxx() methods.
+        
+        Arguments:
+          path -- The path to the dependnecy file.
+        
+        Returns:
+          A list of paths (strings) of all the extra dependencies.
+        """
         try:
             with open(path) as f:
                 items = [s for s in f.read().split('\n') if s]
@@ -20,6 +47,9 @@ class _GccCompiler(object):
             pass
     
     def depfile_args(self, dep_file):
+        """
+        Return a list of arguments to write secondary dependencies to the given dep_file path.
+        """
         return ["-Wp,-MMD,%s" % (dep_file)]
     
     def compile(self, exe, source, dest, dep_file, includes, defines, flags):
@@ -45,12 +75,76 @@ class _GccCompiler(object):
             utils.rm(dep_file)
         
     def compile_c(self, source, dest, dep_file, includes, defines, flags):
+        """
+        Compile a C source file into an object file.
+        
+        Arguments:
+          source   -- The C source file path to compile.
+          dest     -- The output object file path.
+          dep_file -- The path to a dep file that should be written to contain secondary dependencies of the object file
+                      (eg, header files (transitively) included from the source file). This file will be read by
+                      the compiler's load_extra_dependencies() method.
+          includes -- A list of extra include directories.
+          defines  -- A dict of <name>: <value> entries to be used as defines; each entry is equivalent to #define <name> <value>.
+          flags    -- A list of additional flags. This list may contain tuples; to flatten the list, you could use the EMK utils module:
+                      'flattened = utils.flatten(flags)'.
+        """
         self.compile(self.c_path, source, dest, dep_file, includes, defines, flags)
     
     def compile_cxx(self, source, dest, dep_file, includes, defines, flags):
+        """
+        Compile a C++ source file into an object file.
+        
+        Arguments:
+          source   -- The C++ source file path to compile.
+          dest     -- The output object file path.
+          dep_file -- The path to a dep file that should be written to contain secondary dependencies of the object file
+                      (eg, header files (transitively) included from the source file). This file will be read by
+                      the compiler's load_extra_dependencies() method.
+          includes -- A list of extra include directories.
+          defines  -- A dict of <name>: <value> entries to be used as defines; each entry is equivalent to #define <name> <value>.
+          flags    -- A list of additional flags. This list may contain tuples; to flatten the list, you could use the EMK utils module:
+                      'flattened = utils.flatten(flags)'.
+        """
         self.compile(self.cxx_path, source, dest, dep_file, includes, defines, flags)
 
 class Module(object):
+    """
+    EMK module for compiling C and C++ code. Depends on the link module.
+    
+    Classes:
+      GccCompiler - A compiler class that uses gcc/g++ to compile.
+    
+    Properties (inherited from parent scope):
+      compiler     -- The compiler instance that is used to load dependencies and compile C/C++ code.
+      include_dirs -- A list of additional include directories for both C and C++ code.
+      defines      -- A dict of <name>: <value> defines for both C and C++; each entry is equivalent to #define <name> <value>.
+      flags        -- A list of flags for both C and C++. If you have a 'flag' that is more than one argument, pass it as a tuple.
+                      Example: ("-isystem", "/path/to/extra/sys/includes"). Duplicate flags will be removed.
+      source_files -- A list of files that should be included for compilation. Files will be built as C or C++ depending on the file extension.
+      
+      c.exts         -- The list of file extensions (suffixes) that will be considered as C code. The default is [".c"].
+      c.include_dirs -- A list of additional include directories for C code.
+      c.defines      -- A dict of <name>: <value> defines for C.
+      c.flags        -- A list of flags for C.
+      c.source_files -- A list of C files that should be included for compilation (will be built as C code).
+      
+      cxx.exts         -- The list of file extensions (suffixes) that will be considered as C++ code. The default is [".cpp", ".cxx", ".c++", ".cc"].
+      cxx.include_dirs -- A list of additional include directories for C++ code.
+      cxx.defines      -- A dict of <name>: <value> defines for C++.
+      cxx.flags        -- A list of flags for C++.
+      cxx.source_files -- A list of C++ files that should be included for compilation (will be built as C++ code).
+      
+      autodetect  -- Whether or not to autodetect files to build from the scope directory. All files that match the c.exts suffixes
+                     will be compiled as C, and all files that match the cxx.exts suffixes will be compiled as C++. Autodetection
+                     does not take place until the prebuild stage, so that autodetection of generated code can gather as many targets
+                     as possible (see autodetect_from_targets). The default value is True.
+      autodetect_from_targets -- Whether or not to autodetect generated code based on rules defined in the current scope.
+                                 The default value is True.
+      excludes    -- A list of source files to exclude from compilation.
+      non_lib_src -- A list of source files that will not be linked into a library for this directory (passed to the link module).
+      non_exe_src -- A list of source files that will not be linked into an executable, even if they contain a main() function.
+    """
     def __init__(self, scope, parent=None):
         self.GccCompiler = _GccCompiler
         
@@ -67,16 +161,16 @@ class Module(object):
             
             self.source_files = list(parent.source_files)
 
+            self.c.exts = list(parent.c.exts)
             self.c.include_dirs = list(parent.c.include_dirs)
             self.c.defines = parent.c.defines.copy()
             self.c.flags = list(parent.c.flags)
-            self.c.exts = list(parent.c.exts)
             self.c.source_files = list(parent.c.source_files)
             
+            self.cxx.exts = list(parent.cxx.exts)
             self.cxx.include_dirs = list(parent.cxx.include_dirs)
             self.cxx.defines = parent.cxx.defines.copy()
             self.cxx.flags = list(parent.cxx.flags)
-            self.cxx.exts = list(parent.cxx.exts)
             self.cxx.source_files = list(parent.cxx.source_files)
             
             self.autodetect = parent.autodetect
@@ -214,6 +308,19 @@ class Module(object):
             emk.weak_depend(dest, extra_deps)
     
     def do_compile(self, produces, requires, cxx, includes, defines, flags):
+        """
+        Rule function to compile a source file into an object file.
+        
+        The compiler instance will also produce an <object file>.dep file that contains additional dependencies (ie, header files).
+        
+        Arguments:
+          produces -- The path to the object file that will be produced.
+          requires -- The list of dependencies; the source file should be first.
+          cxx      -- If True, the source file will be compiled as C++; otherwise it will be compiled as C.
+          includes -- A list of additional include directories.
+          defines  -- A dict of <name>: <value> entries to be defined (like #define <name> <value>).
+          flags    -- A list of flags to pass to the compiler. Compound flags should be in a tuple, eg: ("-isystem", "/path/to/extra/sys/includes").
+        """
         if not self.compiler:
             raise emk.BuildError("No compiler defined!")
         try:
