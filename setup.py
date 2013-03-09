350619320d86ca81643a26fcf21c2592a5fc6143
kmackay <ken@s010600233295427c.vc.shawcable.net>
2013-03-08 23:03:27 -0800

    Modified setup.py to just install/uninstall.

--- setup.py
+++ setup.py
@@ -1,25 +1,42 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+
 import os
-from setuptools import setup
+import sys
+import emk
 
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+def usage():
+    print("Usage (as root/sudo): 'setup.py install' or 'setup.py uninstall'")
+    sys.exit(1)
 
-setup(
-    name = "emk",
-    version = "0.0.1",
-    author = "Kenneth MacKay",
-    description = ("Build system, written in Python."),
-    license = "BSD",
-    keywords = "build compile make",
-    url = "https://github.com/kmackay/emk",
-    py_modules = ['emk'],
-    package_dir = {'emk.modules': 'modules'},
-    packages = ['emk.modules'],
-    scripts = ['emk'],
-    long_description = read('README.md'),
-    classifiers = [
-        "Development Status :: 3 - Alpha",
-        "Topic :: Software Development :: Build Tools",
-        "License :: OSI Approved :: BSD License"
-    ]
-)
+def install():
+    emk_dir, tail = os.path.split(emk._module_path)
+    bin_path = os.path.join(emk_dir, "emk")
+    if os.path.exists("/usr/bin/emk"):
+        print("/usr/bin/emk already exists; will not overwrite.")
+    else:
+        os.symlink(os.path.join(emk_dir, "emk"), "/usr/bin/emk")
+        print("Created symlink /usr/bin/emk -> %s" % (bin_path))
+
+def uninstall():
+    emk_dir, tail = os.path.split(emk._module_path)
+    bin_path = os.path.join(emk_dir, "emk")
+    try:
+        if os.readlink("/usr/bin/emk") == bin_path:
+            os.remove("/usr/bin/emk")
+            print("Removed /usr/bin/emk")
+            return
+    except OSError:
+        pass
+    print("/usr/bin/emk is not a symlink, or does not point to this instance of emk (%s)" % (bin_path))
+
+if len(sys.argv) != 2:
+    usage()
+
+if sys.argv[1] == "install":
+    install()
+elif sys.argv[1] == "uninstall":
+    uninstall()
+else:
+    usage()
